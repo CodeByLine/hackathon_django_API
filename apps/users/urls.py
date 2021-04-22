2a95df208170df356598dca5511b6ffff88f669e
Y Leventhal <yumei.leventhal@gmail.com>
2021-04-22 17:24:55 -0400

    working on users

---
+++ apps/users/urls.py
@@ -0,0 +1,13 @@
+### urls.py
+
+from django.urls import path, include
+from rest_framework import routers
+# from .views import UserViewSet
+
+router = routers.DefaultRouter()
+# router.register(r'', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+]
